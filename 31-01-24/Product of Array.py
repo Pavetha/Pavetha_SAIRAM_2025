6fb1c377c51c29d3fc5930e33b30c6635903e8d0
Pavetha <157298766+Pavetha@users.noreply.github.com>
2024-02-08 20:44:14 +0530

    Create Product of Array.py

---
+++ 31-01-24/Product of Array.py
@@ -0,0 +1,41 @@
+'''Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.'''
+
+PROGRAM CODE:
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        a,b=1,1
+        c=[]
+        lf=[]
+        rt=[]
+        for i in nums:
+            lf.append(a)
+            a=a*i
+        for i in reversed(nums):
+            rt.append(b)
+            b=b*i
+        for j in range(len(nums)):
+            c.append(lf[j]*rt[len(nums)-1-j])
+        return c
